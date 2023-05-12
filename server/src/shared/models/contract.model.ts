68598534a8a7b060d992654079b6e8e5e1482710
Smithy <it@smiwit.de>
2023-05-12 21:42:12 +0200

    Refactored models to fit nestjs/mongoose module

--- server/src/shared/models/contract.model.ts
+++ server/src/shared/models/contract.model.ts
@@ -1,47 +1,41 @@
-import mongoose from 'mongoose';
-import { IUser } from './user.model';
-import { EWeekday } from '@shared/enums/weekday.enum';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IWorkdayTime } from '@shared/interfaces/workdayTime.interface';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { User } from './user.model';
 
-/**
- * Represents a configuration element in a contract, which defines the planned working hours on a weekday
- */
-export interface IWorkdayTime {
-  day: EWeekday;
-  time: number;
+export type ContractDocument = HydratedDocument<Contract>;
+
+@Schema()
+export class Contract {
+    @Prop({ required: true })
+    num: number;
+
+    @Prop({ required: true })
+    begin: Date;
+
+    @Prop({ required: true })
+    end: Date;
+
+    @Prop({ required: true })
+    weeklyTime: number;
+
+    @Prop({ required: true })
+    timePerWeekday: [IWorkdayTime];
+
+    @Prop({ required: false, type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+    supervisor?: User | string;
+
+    @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+    user: User | string;
+
+    // Function to calculate and set weeklyTime from time values in timePerWeekday
+    setWeeklyTime() : void {
+        let wklyTime: number = 0;
+        this.timePerWeekday.forEach((tpw) => {
+            wklyTime += tpw.time;
+        });
+        this.weeklyTime = wklyTime;
+    }
 }
 
-/**
- * Represents a Contract and its default working time agreement
- */
-export interface IContract {
-  _id?: string;
-  num: number;
-  begin: Date;
-  end: Date;
-  weeklyTime: number;
-  timePerWeekday: [IWorkdayTime];
-  supervisor: IUser | string;
-  user: IUser | string;
-}
-
-const workdayTimeSchema: mongoose.Schema = new mongoose.Schema(
-  {
-    day: { type: EWeekday, required: true },
-    time: { type: Number, required: true },
-  },
-  { _id: false },
-);
-
-const _schema: mongoose.Schema = new mongoose.Schema({
-  num: { type: Number, required: true, unique: true },
-  begin: { type: Date, required: true },
-  end: { type: Date, required: true },
-  weeklyTime: { type: Number, required: true },
-  timePerWeekday: { type: [workdayTimeSchema], required: true, minLength: 1 },
-  supervisor: { type: mongoose.Types.ObjectId, ref: 'User' },
-  user: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
-});
-
-type ContractType = IContract & mongoose.Document;
-
-export default mongoose.model<ContractType>('Contract', _schema);
+export const ContractSchema = SchemaFactory.createForClass(Contract);
