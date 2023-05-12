e6cf9047e9f2fee969e8f77e383399cb54681520
Smithy <it@smiwit.de>
2023-05-12 22:42:45 +0200

    Mongo Skript wegen Fehlern angepasst (noch nicht voll lauffähig)

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
-import { IWorkday } from '@shared/models/workday.model';
 import { TimeSpan } from '@shared/custom/timeSpan';
 import { parse } from 'date-and-time';
+import { Workday } from '@shared/models/workday.model';
+import mongoose from 'mongoose';
 
 @Controller()
 export class AppController {
@@ -17,11 +18,11 @@ export class AppController {
   getWorkdaysForMonth(
     @Param('id') userId: string,
     @Param('month') month: string,
-  ): IWorkday[] {
+  ): Workday[] {
     console.warn('TODO: implement getWorkdaysForMonth');
     return [
       {
-        _id: 'WD.1',
+        _id: new mongoose.Types.ObjectId(),
         start: parse('2023-05-09 09:00', 'YYYY-MM-DD HH:mm'),
         end: parse('2023-05-09 12:00', 'YYYY-MM-DD HH:mm'),
         break: new TimeSpan(0, 0),
@@ -29,7 +30,7 @@ export class AppController {
         user: 'userId',
       },
       {
-        _id: 'WD.2',
+        _id: new mongoose.Types.ObjectId(),
         start: parse('2023-05-10 10:15', 'YYYY-MM-DD HH:mm'),
         end: parse('2023-05-10 15:45', 'YYYY-MM-DD HH:mm'),
         break: new TimeSpan(1, 30),
