e419e864658921d240fa9e8d943102ae2d3cc006
encrypTimM <tim.muehle@stud.hs-bochum.de>
2023-05-13 14:56:55 +0200

    Define REST-Paths

---
+++ server/src/shared/rest/rest-paths.ts
@@ -0,0 +1,5 @@
+export const REST_BASE_URL = 'http://localhost:3000';
+export const REST_WORKDAYS_URL =
+  REST_BASE_URL + '/user/{id}/timetracking/{month}';
+export const REST_CONTRACT_URL = REST_BASE_URL + '/user/{id}/contracts/{month}';
+export const REST_CONTRACTS_URL = REST_BASE_URL + '/user/{id}/contracts';
