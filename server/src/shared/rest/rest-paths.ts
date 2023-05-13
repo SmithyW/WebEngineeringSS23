3fcb94c803c4655f66a6a4fcf83165ce22962bb0
encrypTimM <tim.muehle@stud.hs-bochum.de>
2023-05-13 15:08:18 +0200

    Improve REST-Paths

--- server/src/shared/rest/rest-paths.ts
+++ server/src/shared/rest/rest-paths.ts
@@ -1,5 +1,41 @@
+import { Month } from '@shared/enums/month.enum';
+
 export const REST_BASE_URL = 'http://localhost:3000';
-export const REST_WORKDAYS_URL =
-  REST_BASE_URL + '/user/{id}/timetracking/{month}';
-export const REST_CONTRACT_URL = REST_BASE_URL + '/user/{id}/contracts/{month}';
-export const REST_CONTRACTS_URL = REST_BASE_URL + '/user/{id}/contracts';
+
+export class REST_WORKDAYS_URL {
+  readonly url = REST_BASE_URL + '/user/{id}/timetracking/workdays';
+
+  public resolve(userid: string): string {
+    return this.url.replace('{id}', userid);
+  }
+
+  public postData(month: Month, year: number): any {
+    return {
+      month: month,
+      year: year,
+    };
+  }
+}
+
+export class REST_CONTRACT_URL {
+  readonly url = REST_BASE_URL + '/user/{id}/contracts';
+
+  public resolve(userid: string): string {
+    return this.url.replace('{id}', userid);
+  }
+
+  public postData(month: Month, year: number): any {
+    return {
+      month: month,
+      year: year,
+    };
+  }
+}
+
+export class REST_CONTRACTS_URL {
+  readonly url = REST_BASE_URL + '/user/{id}/contracts';
+
+  public resolve(userid: string): string {
+    return this.url.replace('{id}', userid);
+  }
+}
