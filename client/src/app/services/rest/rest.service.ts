725662ab9dae82ff9baf1c5d2d07108b3d863638
encrypTimM <tim.muehle@stud.hs-bochum.de>
2023-06-06 15:43:51 +0200

    Update REST

--- client/src/app/services/rest/rest.service.ts
+++ client/src/app/services/rest/rest.service.ts
@@ -6,40 +6,96 @@ import { Month } from '@shared/enums/month.enum';
 import { Contract } from '@shared/models/contract.model';
 import * as paths from '@shared/rest/rest-paths';
 import { AuthenticationService } from '../authentication/authentication.service';
+import { User } from '@shared/models/user.model';
+import { UserData } from '@shared/custom/types';
+import { ContractData } from '@shared/custom/types';
+import { WorkdayData } from '@shared/custom/types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
   private static readonly REST_BASE_URL = "http://localhost:3000";
-  private static readonly REST_WORKDAYS_URL = RestService.REST_BASE_URL + "/user/{id}/timetracking/{month}";
-  private static readonly REST_CONTRACT_URL = RestService.REST_BASE_URL + "/user/{id}/contracts/{month}";
 
   constructor(
     private httpClient: HttpClient,
     private authServcie: AuthenticationService,
   ) { }
 
-  public fetchWorkdays(month: Month, year: number): Observable<Workday[]> {
+  public fetchWorkdays(month: Month, year: number): Observable<WorkdayData[]> {
     const user = this.authServcie.getUser();
     if (!user?._id){
       throw new Error('A user must be logged in!');
     }
 
     const url = paths.REST_FETCH_WORKDAYS_URL.resolve(user._id.toString());
-    const postData = paths.REST_FETCH_WORKDAYS_URL.postData(month, year);
-    return this.httpClient.post<Workday[]>(url, postData);
+    const queryParams = paths.REST_FETCH_WORKDAYS_URL.queryParams(month, year);
+    return this.httpClient.get<WorkdayData[]>(url, {
+      params: queryParams
+    });
   }
 
-  public fetchContract(month: Month, year: number): Observable<Contract> {
+  public updateWorkday(workday: WorkdayData): Observable<WorkdayData> {
+    const user = this.authServcie.getUser();
+    if (!user?._id){
+      throw new Error('A user must be logged in!');
+    }
+
+    const url = paths.REST_UPDATE_WORKDAY_URL.resolve(user._id.toString(), workday);
+    return this.httpClient.post<WorkdayData>(url, workday);
+  }
+
+  public fetchContract(month: Month, year: number): Observable<ContractData> {
     const user = this.authServcie.getUser();
     if (!user?._id){
       throw new Error('A user must be logged in!');
     }
 
     const url = paths.REST_FETCH_CONTRACT_URL.resolve(user._id.toString());
-    const postData = paths.REST_FETCH_CONTRACT_URL.postData(month, year);
-    return this.httpClient.post<Contract>(url, postData);
+    const queryParams = paths.REST_FETCH_CONTRACT_URL.queryParams(month, year);
+    return this.httpClient.get<ContractData>(url, {
+      params: queryParams
+    });
+  }
+
+  public updateContract(contract: ContractData): Observable<ContractData> {
+    const user = this.authServcie.getUser();
+    if (!user?._id){
+      throw new Error('A user must be logged in!');
+    }
+
+    const url = paths.REST_UPDATE_CONTRACT_URL.resolve(user._id.toString(), contract);
+    return this.httpClient.post<ContractData>(url, contract);
+  }
+
+  public fetchContracts(): Observable<ContractData[]> {
+    const user = this.authServcie.getUser();
+    if (!user?._id){
+      throw new Error('A user must be logged in!');
+    }
+
+    const url = paths.REST_FETCH_CONTRACT_URL.resolve(user._id.toString());
+    return this.httpClient.get<ContractData[]>(url);
+  }
+
+  public fetchUser(): Observable<UserData> {
+    const user = this.authServcie.getUser();
+    if (!user?._id){
+      throw new Error('A user must be logged in!');
+    }
+
+    const url = paths.REST_FETCH_USER_URL.resolve(user._id.toString());
+    return this.httpClient.get<UserData>(url);
+  }
+
+  public updateUser(userData: UserData): Observable<UserData> {
+    const user = this.authServcie.getUser();
+    if (!user?._id){
+      throw new Error('A user must be logged in!');
+    }
+
+    const url = paths.REST_UPDATE_USER_URL.resolve(user._id.toString());
+    return this.httpClient.post<UserData>(url, userData);
   }
 
 }
--- server/src/shared/rest/rest-paths.ts
+++ server/src/shared/rest/rest-paths.ts
@@ -1,5 +1,9 @@
+import { WorkdayData } from '@shared/custom/types';
+import { ContractData } from '@shared/custom/types';
 import { MonthYear } from '@shared/custom/types';
 import { Month } from '@shared/enums/month.enum';
+import { Contract } from '@shared/models/contract.model';
+import { Workday } from '@shared/models/workday.model';
 
 export const REST_BASE_URL = 'http://localhost:3000';
 
@@ -10,11 +14,21 @@ export class REST_FETCH_WORKDAYS_URL {
     return REST_FETCH_WORKDAYS_URL.url.replace('{id}', userid);
   }
 
-  public static postData(month: Month, year: number): MonthYear {
+  public static queryParams(month: Month, year: number): MonthYear {
     return {
       month: month,
       year: year,
     };
+  }
+}
+
+export class REST_UPDATE_WORKDAY_URL {
+  static readonly url = REST_BASE_URL + '/user/{userId}/workdays/{id}';
+
+  public static resolve(userid: string, workday: WorkdayData): string {
+    return REST_FETCH_WORKDAYS_URL.url
+      .replace('{userId}', userid)
+      .replace('{id}', workday._id.toString());
   }
 }
 
@@ -25,7 +39,7 @@ export class REST_FETCH_CONTRACT_URL {
     return REST_FETCH_CONTRACT_URL.url.replace('{id}', userid);
   }
 
-  public static postData(month: Month, year: number): MonthYear {
+  public static queryParams(month: Month, year: number): MonthYear {
     return {
       month: month,
       year: year,
@@ -33,14 +47,35 @@ export class REST_FETCH_CONTRACT_URL {
   }
 }
 
-export class REST_FETCH_CONTRACTS_URL {
-  static readonly url = REST_BASE_URL + '/user/{id}/contracts/all';
+export class REST_UPDATE_CONTRACT_URL {
+  static readonly url = REST_BASE_URL + '/user/{userId}/contracts/{id}';
 
-  public static resolve(userid: string): string {
-    return REST_FETCH_CONTRACTS_URL.url.replace('{id}', userid);
+  public static resolve(userid: string, contract: ContractData): string {
+    return REST_FETCH_CONTRACT_URL.url
+      .replace('{userId}', userid)
+      .replace('{id}', contract._id.toString());
   }
 
-  public static postData(): any {
-    return {};
+  public static queryParams(month: Month, year: number): MonthYear {
+    return {
+      month: month,
+      year: year,
+    };
+  }
+}
+
+export class REST_FETCH_USER_URL {
+  static readonly url = REST_BASE_URL + '/user/{id}';
+
+  public static resolve(userid: string): string {
+    return REST_FETCH_WORKDAYS_URL.url.replace('{id}', userid);
+  }
+}
+
+export class REST_UPDATE_USER_URL {
+  static readonly url = REST_BASE_URL + '/user/{id}';
+
+  public static resolve(userid: string): string {
+    return REST_FETCH_WORKDAYS_URL.url.replace('{id}', userid);
   }
 }
